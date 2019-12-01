0902c0fb22ccb9ee204bbcd73af1e2904bc9fd0d
Rishi Sharma <39599919+kampaitees@users.noreply.github.com>
2019-12-01 23:10:36 +0530

    Add files via upload

---
+++ Constructor/assigment_constructor.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+class Cube
+{
+public:
+    Cube();
+    Cube(const Cube &obj);
+    Cube &operator=(const Cube &obj);
+    double getVolume();
+    double getSurfaceArea();
+    void setLength(double length);
+
+private:
+    double length_;
+};
+
+Cube::Cube()
+{
+    length_ = 1;
+    cout << "Default Constructor Invoked!!!" << endl;
+}
+
+Cube::Cube(const Cube &obj)
+{
+    length_ = obj.length_;
+    cout << "Copy Constructor Invoked!!!" << endl;
+}
+
+Cube &Cube::operator=(const Cube &obj)
+{
+    length_ = obj.length_;
+    cout << "Assigment Constructor Invoked!!!" << endl;
+    return *this;
+}
+
+int main()
+{
+    Cube c;
+    Cube myCube;
+
+    myCube = c;
+
+    return 0;
+}
+
+/* Output
+Default Constructor Invoked!!!
+Default Constructor Invoked!!!
+Assignment Constructor Invoked!!!
+*/
