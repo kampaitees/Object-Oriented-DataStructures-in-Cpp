a803c0b4e09a6a1ec23e21381b68e3be1fe9827f
Rishi Sharma <39599919+kampaitees@users.noreply.github.com>
2019-12-01 22:47:01 +0530

    Add files via upload

---
+++ Constructor/copy_cons_3.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+class Cube
+{
+public:
+    Cube();
+    Cube(const Cube &obj);
+    double getVolume();
+    double getSurfaceArea();
+    void setLength(double length);
+
+private:
+    double length_;
+};
+
+Cube::Cube()
+{
+    length_ = 1;
+    cout << "Default Constructor Invoked!!!" << endl;
+}
+
+Cube::Cube(const Cube &obj)
+{
+    length_ = obj.length_;
+    cout << "Copy Constructor Invoked!!!" << endl;
+}
+
+int main()
+{
+    Cube c;
+    Cube myCube = c;
+
+    return 0;
+}
+
+/* Output
+Default Constructor Invoked!!!
+Copy Constructor Invoked!!!
+*/
